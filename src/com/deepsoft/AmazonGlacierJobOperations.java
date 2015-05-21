f5d5000b6cd9fecf0d5fd22c877c911d2b8d5705
Robert Heller <heller@deepsoft.com>
2015-05-21 20:49:52 +0000

    Started on job ops.

---
+++ src/com/deepsoft/AmazonGlacierJobOperations.java
@@ -0,0 +1,66 @@
+/****************************************************************************
+ *
+ *  System        : 
+ *  Module        : 
+ *  Object Name   : $RCSfile$
+ *  Revision      : $Revision$
+ *  Date          : $Date$
+ *  Author        : $Author$
+ *  Created By    : Robert Heller
+ *  Created       : Thu May 21 15:02:55 2015
+ *  Last Modified : <150521.1532>
+ *
+ *  Description	
+ *
+ *  Notes
+ *
+ *  History
+ *	
+ ****************************************************************************
+ *
+ *    Copyright (C) 2015  Robert Heller D/B/A Deepwoods Software
+ *			51 Locke Hill Road
+ *			Wendell, MA 01379-9728
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * 
+ *
+ ****************************************************************************/
+
+
+package com.deepsoft;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.PropertiesCredentials;
+import com.amazonaws.services.glacier.AmazonGlacierClient;
+import com.amazonaws.services.glacier.model.InitiateJobRequest;
+import com.amazonaws.services.glacier.model.InitiateJobResult;
+import com.amazonaws.services.glacier.model.JobParameters;
+import com.amazonaws.services.glacier.model.InventoryRetrievalJobInput;
+
+
+public class AmazonGlacierJobOperations {
+    public static InitiateJobResult initiateJob(AmazonGlacierClient client,String vaultName,JobParameters jobParams) throws Exception {
+        InitiateJobRequest request = new InitiateJobRequest()
+              .withVaultName(vaultName)
+              .withJobParameters(jobParams);
+        return client.initiateJob(request);
+    }
+}
+
+
