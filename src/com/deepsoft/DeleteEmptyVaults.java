11a23e471882e09f12d5b39abb5b82206b9a2f05
Robert Heller <heller@deepsoft.com>
2023-11-26 17:49:42 +0000

    Add Delete Empty Vaults program.

---
+++ src/com/deepsoft/DeleteEmptyVaults.java
@@ -0,0 +1,87 @@
+/****************************************************************************
+ *
+ *  System        : 
+ *  Module        : 
+ *  Object Name   : $RCSfile$
+ *  Revision      : $Revision$
+ *  Date          : $Date$
+ *  Author        : $Author$
+ *  Created By    : Robert Heller
+ *  Created       : Sun Nov 26 12:19:44 2023
+ *  Last Modified : <231126.1242>
+ *
+ *  Description	
+ *
+ *  Notes
+ *
+ *  History
+ *	
+ ****************************************************************************
+ *
+ *    Copyright (C) 2023  Robert Heller D/B/A Deepwoods Software
+ *			51 Locke Hill Road
+ *			Wendell, MA 01379-9728
+ *
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * 
+ *
+ ****************************************************************************/
+
+
+package com.deepsoft;
+
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.lang.*;
+import java.text.*;
+import org.w3c.dom.*;
+import com.deepsoft.*;
+
+public class DeleteEmptyVaults extends BackupVault {
+    private static File GlacierVaultDB_File;
+    private static SiteConfig configuration;
+    public DeleteEmptyVaults() throws Exception {
+        super(GlacierVaultDB_File);
+    }
+    public void deleteemptyvaults() throws Exception {
+        Element vaultsnode = getvaultnode();
+        NodeList vaults = vaultsnode.getElementsByTagName("vault");
+        int i;
+        for (i=0; i < vaults.getLength(); i++) {
+            Element vault = (Element) vaults.item(i);
+            NodeList archives = vault.getElementsByTagName("archive");
+            int archivecount = archives.getLength();
+            if (archivecount == 0) {
+                String tape = vault.getAttribute("name");
+                try {
+                    deletevault(tape);
+                    System.out.println("Vault "+tape+" deleted from the Glacier");
+                } catch (Exception e) {
+                    System.err.println("Exception deleting vault: "+e.getMessage());
+                }
+            }
+        }
+        savedb(GlacierVaultDB_File);
+    }
+    public static void main(String args[]) throws Exception {
+        configuration = new SiteConfig();
+        GlacierVaultDB_File = new File(configuration.GlacierVaultDB_FileName());
+        DeleteEmptyVaults DEV = new DeleteEmptyVaults();
+        DEV.deleteemptyvaults();
+    }
+}
+
